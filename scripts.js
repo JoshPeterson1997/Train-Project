f47e637bab5c195d3475b52824e2cf7838fbd2bc
Joshua Peterson <petersonjoshua613@yahoo.com>
2021-05-11 19:49:10 -0500

    Added function and dom code in js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('form');
+const p = document.querySelector('p');
+const students = [];
 
+function getRandomIntInclusive(min, max) {
+  const currentMin = Math.ceil(min);
+  const currentMax = Math.floor(max);
+  return Math.floor(Math.random() * (currentMax - currentMin + 1) + currentMin); // The maximum is inclusive and the minimum is inclusive
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const newStudent = Object.fromEntries(new FormData(event.target));
+
+  students.push(newStudent);
+});
+
+document.querySelector('.button--jumbo').addEventListener('click', () => {
+  // Get a random student object in the `students` array
+  const randomName = students[getRandomIntInclusive(0, students.length - 1)];
+
+  // Log the first and last name of the student using template literals
+  p.innerText = `${randomName.fname} ${randomName.lname}`;
+});
